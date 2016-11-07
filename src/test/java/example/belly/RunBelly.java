861d943304fa3606f6ace25a91b97d32b6edc24a
Les Mear <leslie.mear@qa.com>
2016-11-07 17:32:20 +0000

    Les's Branch

--- src/test/java/example/belly/RunBelly.java
+++ src/test/java/example/belly/RunBelly.java
@@ -9,7 +9,7 @@ import uk.gov.homeoffice.virtuoso.framework.bdd.IntegratedCucumber;
     @CucumberOptions(
         features = "src/test/resources/features/belly/belly.feature",
         format = {"pretty", "html:virtuoso-test-results/belly", "json:virtuoso-test-results/belly.json"}
-        , tags = {"@growl, @slient"}
+        , tags = {"@growl, @silent"}
     )
 
     public class RunBelly {
