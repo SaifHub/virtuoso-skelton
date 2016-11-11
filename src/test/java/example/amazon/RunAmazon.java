8d383694719865d8f797212efcef78c440b006a8
Les Mear <leslie.mear@qa.com>
2016-11-11 11:17:01 +0000

    Tests Update

--- src/test/java/example/amazon/RunAmazon.java
+++ src/test/java/example/amazon/RunAmazon.java
@@ -8,8 +8,12 @@ import uk.gov.homeoffice.virtuoso.framework.bdd.IntegratedCucumber;
 @RunWith(IntegratedCucumber.class)
 
 @CucumberOptions(
-        features = "src/test/resources/features/amazon",
-        format = "pretty"
+		features = "src/test/resources/features/amazon",
+		format = {"pretty",
+				"html:virtuoso-test-results/amazon", 
+				"json:virtuoso-test-results/amazon.json"
+        		},
+        monochrome = false
 )
 
 public class RunAmazon
