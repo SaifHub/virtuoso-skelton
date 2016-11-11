912e74638bf7711ddda2af163d6e4a0370a6dc3b
Philip worrall <Philip worrall>
2016-11-11 15:22:00 +0000

    bugfixes

--- src/test/java/example/amazon/RunAmazon.java
+++ src/test/java/example/amazon/RunAmazon.java
@@ -6,7 +6,10 @@ import cucumber.api.CucumberOptions;
 import uk.gov.homeoffice.virtuoso.framework.bdd.IntegratedCucumber;
 
 @RunWith(IntegratedCucumber.class)
-@CucumberOptions(features = "src/test/resources/features/amazon")
+@CucumberOptions(features = "src/test/resources/features/amazon",
+format = {"pretty", "html:virtuoso-test-results/amazon", "json:virtuoso-test-results/amazon.json"}
+)
+
 
 public class RunAmazon {
 
