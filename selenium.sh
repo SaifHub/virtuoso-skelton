ac9634f40064eda2cfc75e4c75f97e2482ee9a60
Les Mear <leslie.mear@qa.com>
2016-11-25 14:57:32 +0000

    update

--- selenium.sh
+++
@@ -1,3 +0,0 @@
-docker run -d -p 4444:4444 --name selenium-hub selenium/hub:3.0.1
-docker run -d --link selenium-hub:hub selenium/node-chrome:3.0.1-aluminum
-docker run -d --link selenium-hub:hub selenium/node-firefox:3.0.1-aluminum
