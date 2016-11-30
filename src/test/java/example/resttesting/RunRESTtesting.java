20c4933c49b7dee8bbd54baf2ee85e5578b98d6c
Philip worrall <Philip worrall>
2016-11-30 17:24:46 +0000

    added glue, no more undefined

--- src/test/java/example/resttesting/RunRESTtesting.java
+++ src/test/java/example/resttesting/RunRESTtesting.java
@@ -7,7 +7,8 @@ import uk.gov.homeoffice.virtuoso.framework.bdd.IntegratedCucumber;
 
 @RunWith(IntegratedCucumber.class)
 @CucumberOptions(features = "src/main/java/RESTTesting/webversion.feature",
-format = {"pretty", "html:virtuoso-test-results/resttesting", "json:virtuoso-test-results/resttesting.json"}
+format = {"pretty", "html:virtuoso-test-results/resttesting", "json:virtuoso-test-results/resttesting.json"},
+glue ="RESTTesting"
 )
 
 
